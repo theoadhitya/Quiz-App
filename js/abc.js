f323aee54a3b364ccd058b16c1cfa96279dbce95
theoadhitya <theoadhitya.sw@gmail.com>
2019-03-11 18:21:14 +0700

    show question list

--- js/abc.js
+++ js/abc.js
@@ -12,9 +12,26 @@ function loadQ() {
   req.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var response = JSON.parse(this.responseText);
-      document.getElementById("root").innerHTML = response[0].question;
+      contentList(response);
+      // document.getElementById("root").innerHTML = response[1].question;
     }
   }
-  req.open("GET", "data/question_data.php", true);
+  req.open("GET", "data/08-03-2019.json", true);
   req.send();
+}
+
+function contentList(response) {
+  var output = "<h1></h1>";
+  for (let i=0; i < response.length; i++){
+    output += "<li>";
+    output += "<h3>" + response[i].question + "</h3>";
+    output += "<input type='radio' name= '" + response[i].question_id +"' value='option_A'>" + response[i].option_A + "<br>";
+    output += "<input type='radio' name= '" + response[i].question_id +"' value='option_B'>" + response[i].option_B + "<br>";
+    output += "<input type='radio' name= '" + response[i].question_id +"' value='option_C'>" + response[i].option_C + "<br>";
+    output += "<input type='radio' name= '" + response[i].question_id +"' value='option_D'>" + response[i].option_D + "<br>";
+    output += "<input type='radio' name= '" + response[i].question_id +"' value='option_E'>" + response[i].option_E + "<br>";
+    output += "</li>";
+  }
+  output += "<br>" + "<input type='button' value='Submit Answers'>";
+      document.getElementById("quiz-content").innerHTML = output;
 }
