aadf6a0071e01d3856fb0576acb00f511731f073
theoadhitya <theoadhitya.sw@gmail.com>
2019-03-11 19:26:16 +0700

    Adding insert answer control function

---
+++ data/answer_data.php
@@ -0,0 +1,18 @@
+<?php
+include "../controller/config.php";
+include "../controller/answer.php";
+
+$test_id = $_POST['test_id'];
+$student_id = $_POST['student_id'];
+$question_id = $_POST['question_id'];
+$answer = $_POST['answer'];
+
+$post_answer = new Answer($conn, $test_id, $student_id, $question_id, $answer);
+
+if ($post_answer->insert()) {
+    echo "Success";
+} else {
+    echo "Some Problems Occured";
+}
+
+?>
