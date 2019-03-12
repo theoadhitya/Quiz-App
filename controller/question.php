e75fe201b9b9dc67f92eb17146ddd4d1c4f23c01
theoadhitya <theoadhitya.sw@gmail.com>
2019-03-12 11:21:44 +0700

    Added basic inserting question for dashboard

--- controller/question.php
+++ controller/question.php
@@ -7,7 +7,7 @@ class Question {
         $this->conn = $conn;
     }
 
-    public function getAllQuestion() {
+    function getAllQuestion() {
         $query = 'SELECT * FROM '.$this->table_name;
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -17,6 +17,27 @@ class Question {
             return json_encode($questions, JSON_PRETTY_PRINT);
         }
         return false;
+    }
+
+    function insertQuestion($topic, $question_id, $question, $option_A, $option_B, $option_C, $option_D, $option_E, $answer) {
+        $query = 'INSERT INTO ' . $this->table_name .
+            ' VALUE(:topic, :question_id, :question, :option_A, :option_B, :option_C, :option_D, :option_E, :answer)';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':topic', $topic);
+        $stmt->bindParam(':question_id', $question_id);
+        $stmt->bindParam(':question', $question);
+        $stmt->bindParam(':option_A', $option_A);
+        $stmt->bindParam(':option_B', $option_B);
+        $stmt->bindParam(':option_C', $option_C);
+        $stmt->bindParam(':option_D', $option_D);
+        $stmt->bindParam(':option_E', $option_E);
+        $stmt->bindParam(':answer', $answer);
+        
+        if ($stmt->execute()) {
+            echo "Success";
+        } else {
+            echo "Insert Failed";
+        }
     }
 }
 ?>
