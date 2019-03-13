1a664775db3b8e347cb5601aaabe5292df9cdcb5
xxxawenxxx <awenm10@gmail.com>
2019-03-13 18:22:52 +0700

    navbar

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+  <link rel="stylesheet" href="index.css">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, intial-scale=1" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>Quiz App Test</title>
   </head>
 
   <body>
+  <nav class="navbar">
+      <div class="container-fluid">
+        <div class ="navbar-header">
+          <a class ="brandlogo" href ="#">SareJeroQuiz</a>
+        </div>
+          <ul class ="nav navbar-nav navbar-right">
+              <li><a href ="#"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+              <li><a href ="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </ul>
+      </div>
+      </div>
+      </div>
+    </nav>
     <div id="root">
       <p id="test"></p>
       <ol id="quiz-content"></ol>
